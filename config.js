fadf520f6e9bf8ba9f5e668de42c9c127e473829
ReRamp <aidan@detonatorgames.com>
2017-05-31 22:34:17 -0700

    *slid into the dms*

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         'DGDUzGN5UxoExnUBshDozmqtK',
-  consumer_secret:      'mS7vFvIB5LaNIzAbMp11y5rXZg4eBGlGtEnjf0o0mJSnQkwqCV',
-  access_token:         '847289190882525185-PRKZlvUKaopxNGWESpXghVCoGbTcm77',
-  access_token_secret:  'TC1zD0jvzr2AR4Z0Ixe4vRpxHf0LeQUX8BaxN0YkoSjdh',
+  consumer_key:         'aqrPjeDsQFLQAwk4iRxWikmJl',
+  consumer_secret:      'naDLfDtmnGGVDDa06PAJA9S4P1ZLSqgrb0til2shmBZJm6Ippe',
+  access_token:         '847289190882525185-rNB8g04rc1abseUqhEoqXG5KJWRGvMj',
+  access_token_secret:  'HyCdbqxo1XdhLuqb04LPGjLBSSa2zpTh6BLyH2WenIbX7',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
